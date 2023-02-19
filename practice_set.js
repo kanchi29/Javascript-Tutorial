2c9aad0e9751f87679fbc72202c9ecefc7f91a69
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-19 21:06:27 +0530

    Practice Questions

---
+++ practice_set.js
@@ -0,0 +1,27 @@
+//Add a number to a string
+let a = "Kanchi";
+let b = 28;
+console.log(a+b);
+
+//Using typeof to find datatype
+console.log(typeof a);
+
+//Creating a const object
+const c = {
+    name:"Kanchi",
+    class: "IT1",
+    adm_no:"20IT136"
+}
+console.log(c["class"]);
+
+//Adding a new key in const object
+c['room']="116";
+console.log(c);
+
+//Creating a word meaning dictionary of 5 words
+const dict = {
+    appreciate: "Be grateful",
+    yakka:"Doing Hard Work",
+    paint: "To add colors to something"
+}
+console.log(dict);
