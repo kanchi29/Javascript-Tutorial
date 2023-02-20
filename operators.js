231da3a34717f2d2b9643da6d0331b339cf7a54d
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-20 23:41:49 +0530

    Assignment Operators

--- operators.js
+++ operators.js
@@ -1,4 +1,6 @@
 console.log("Operators in js");
+
+//Arithmetic operators
 let a= 45;
 let b= 4;
 console.log("a+b=", a+b);
@@ -10,3 +12,11 @@ console.log("a%b=", a%b);
 console.log("a++=", a++);
 console.log("a--=", a--);
 console.log("++a=", ++a);
+
+//Assignment Operators
+let c = 25;
+console.log("c = ", c += 5);
+console.log("c = ", c -= 5);
+console.log("c = ", c *= 2);
+console.log("c = ", c /= 5);
+console.log("c = ", c **= 2);
