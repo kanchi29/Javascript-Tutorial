0a1af8acda94035e52c694037d7e08c9ab472185
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-21 23:46:06 +0530

    For loops

---
+++ 5_loops.js
@@ -0,0 +1,27 @@
+//for loop
+sum = 0;
+let n = parseInt(prompt("Enter a number"));
+for(let i=1; i<n+1;i++){
+    sum += i;
+}
+console.log("Sum of first "+n+" numbers is "+sum); 
+
+//for in loop
+let obj = {
+    harry: 02,
+    louis: 25,
+    niall: 22,
+    liam: 13,
+    zayn: 12
+}
+for(let a in obj){
+    console.log(obj);
+}
+for(let a in obj){
+    console.log(obj)
+}
+
+//for of loop
+for(let b of "Harry"){
+    console.log(b);
+}
