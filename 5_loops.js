ec66d13ca7c93027b7b2de94129aaf305d825ac1
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-21 23:49:00 +0530

    For loops executed

--- 5_loops.js
+++ 5_loops.js
@@ -1,4 +1,4 @@
-//for loop
+/*//for loop
 sum = 0;
 let n = parseInt(prompt("Enter a number"));
 for(let i=1; i<n+1;i++){
@@ -19,7 +19,7 @@ for(let a in obj){
 }
 for(let a in obj){
     console.log(obj)
-}
+} */
 
 //for of loop
 for(let b of "Harry"){
