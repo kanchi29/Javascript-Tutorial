3ed18fca0d43533a72546078762212e574c04453
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-22 23:41:14 +0530

    functions

---
+++ 7_functions.js
@@ -0,0 +1,12 @@
+function calc_avg(x,y){
+    return (x+y)/2;
+}
+let a=5;
+let b=7;
+console.log("Average of a and b is: ", calc_avg(a,b));
+
+//Another way of writing functions
+const sum = (p,q)=>{
+    return p+q;
+}
+console.log(sum(10,20));
