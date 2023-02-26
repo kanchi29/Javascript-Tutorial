490d1eaa5cc486d8158d9b0a9166f8e35e132f81
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-26 19:56:07 +0530

    Practice set questions

--- 8_practice_set.js
+++ 8_practice_set.js
@@ -1,8 +1,33 @@
 //Problem 1
-let obj={
+let marks={
     harry:90,
     rohan:70,
     aakash:7
 }
-for(let a in obj){
-    console.log(obj[a]);
+ for (let i=0;i<Object.keys(marks).length;i++){
+   // console.log("The marks of " + Object.keys(marks)[i] + " are " +marks[Object.keys(marks)[i]])
+ }
+
+// Problem 2
+
+for (let key in marks){
+   // console.log("The marks of "+ key + " are " + marks[key])
+}
+
+//Problem 3
+let cn=28;
+let i;
+i = prompt("Enter a number");
+while(i!=cn){
+    console.log("Try Again");
+    i = prompt("Enter a number");
+}
+console.log("You have entered the correct number");
+
+//Problem 4
+const mean = (a,b,c,d,e)=>{
+    let m;
+    m=(a+b+c+d+e)/5
+    return m;
+}
+console.log(mean(1,2,3,4,5));
