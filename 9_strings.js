2d4b5e6fc49f9721a55ed32a7cf825d8ee815185
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-26 20:56:17 +0530

    Strings

---
+++ 9_strings.js
@@ -0,0 +1,13 @@
+// String
+let a = "Kanchi ";
+let b='Devanshi';
+console.log(a + b);
+
+//Template Literal
+let c = `I am a girl`;
+let d = `${a} is a girl `
+console.log(d);
+
+//Escape Sequence Character
+let e = 'Bana\'na' // used to print special character when javascript might get confused
+console.log(e);
