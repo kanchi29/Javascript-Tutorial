9aef27ee62f5dfa5a88b563d950b88d5d585ff48
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-02-26 22:59:55 +0530

    Few String methods

---
+++ 10_String_Methods.js
@@ -0,0 +1,21 @@
+ let a = "Kanchi"
+ console.log(a.length) // Length is a property
+ let b = "Kanchi \'"
+ console.log(b.length) //Length of \' is 1 only
+ console.log(a.toUpperCase()); // toUpperCase is a function
+ console.log(a.slice(2,4))
+ console.log(a.slice(2,4))
+ console.log(a.replace("K","S"))
+ let c = "Devanshi"
+ console.log(a.concat(" is a friend of ",c))
+ let d ="   Meena   "
+ console.log(d)
+ console.log(d.trim()) 
+ let x = "Sneha"
+ console.log(x[1])
+
+ for(let i in x){
+    console.log(x[i])
+ }
+
+ // String is immutable, it cannot be changed. 
