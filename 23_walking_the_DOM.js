a4a9f38102c5f10773600e93a2c457e95c6170cc
kanchi29 <85997407+kanchi29@users.noreply.github.com>
2023-03-12 21:32:06 +0530

    Walking the DOM, Autocorrection

---
+++ 23_walking_the_DOM.js
@@ -0,0 +1,8 @@
+// DOM tree refers to the HTML page where all the nodes are objects.
+// 3 main types of noides: Text nodes, Element nodes, Comment nodes
+//<html> is at the root, <head> and <body> are its children 
+//text node is always the leaf node of the tree
+
+
+//Autocorrection is when some erroneous html is encountered by the browser it tries to avoid or correct tat error
+// Ex- if something is written is written outside a body tag browser will addit inside the body tag. 
