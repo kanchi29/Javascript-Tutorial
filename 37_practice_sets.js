04aad7051f3abf7a3d000fe92ec2107b88d4d0ef
kanchi29 <kanchisri29@gmail.com>
2023-04-11 23:56:22 +0530

    practice set

---
+++ 37_practice_sets.js
@@ -0,0 +1,13 @@
+// //If we add even using event listener instead of using href
+
+// document.getElementById("btn1").addEventListener('click',function(){
+//     window.location = 'https://www.google.com'
+//     window.focus();
+// })
+
+// //Fetching data of a website every few seconds
+// setInterval(async function(){
+//     let url="https://jsonplaceholder.typicode.com/todos/1"
+//     console.log(await fetchContent(url))
+// }, 3000);
+
