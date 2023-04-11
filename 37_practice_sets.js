dec321fb35d5be262984277a4cd9e8d7541f418d
kanchi29 <kanchisri29@gmail.com>
2023-04-11 23:57:16 +0530

    practice 37_practice_sets

--- 37_practice_sets.js
+++ 37_practice_sets.js
@@ -1,13 +1,13 @@
-// //If we add even using event listener instead of using href
+//If we add even using event listener instead of using href
 
-// document.getElementById("btn1").addEventListener('click',function(){
-//     window.location = 'https://www.google.com'
-//     window.focus();
-// })
+document.getElementById("btn1").addEventListener('click',function(){
+    window.location = 'https://www.google.com'
+    window.focus();
+})
 
-// //Fetching data of a website every few seconds
-// setInterval(async function(){
-//     let url="https://jsonplaceholder.typicode.com/todos/1"
-//     console.log(await fetchContent(url))
-// }, 3000);
+//Fetching data of a website every few seconds
+setInterval(async function(){
+    let url="https://jsonplaceholder.typicode.com/todos/1"
+    console.log(await fetchContent(url))
+}, 3000);
 
