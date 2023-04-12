2363a552b2b700fd53fbcdabf54243f4a32b4fa5
kanchi29 <kanchisri29@gmail.com>
2023-04-12 21:26:17 +0530

    Promises introduction

---
+++ 39_promises.js
@@ -0,0 +1,27 @@
+//promises- promise of code execution. it is a solution of callback hell.
+//the code either executes or fails, in either case the subscriber will be informed.
+//syntax is :
+// let promise = new promise(function(resolve,reject){
+    //executor
+//});
+
+let promise = new Promise(function(resolve,reject){
+    alert("HELLO");
+    resolve(56)
+})
+
+console.log('Hello one')
+setTimeout(function(){
+    console.log("Hello Two in 3 seconds")
+},3000)
+
+console.log("My name is " + "Hello three");
+console.log(promise);
+
+// Use of promise is if we want to do the following tasks parallely:
+// Fetch google.com homepage ==> when its done, do console.log("google.com homepage done")
+// Fetch data from a data Api 
+// Fetch pictures from the Server
+// Print downloading
+// Rest of the Script 
+// All of this can be done parallely and in an organized manner using promises
