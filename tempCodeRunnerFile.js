e1a5707aeb8c0ae6eee973c544d2fd0aea2e2300
kanchi29 <kanchisri29@gmail.com>
2024-09-01 20:35:01 +0530

    switch and if-else conditional statements

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,34 @@
+let a= prompt("What is your age?")
+// switch(a){
+//     case '1': console.log("Your age is 1")
+//     break;
+//     case '2': console.log("Your age is 2")
+//     break;
+//     case '3': console.log("Your age is 3")
+//     break;
+//     default: console.log("Special case")
+//     break;
+// }
+
+// //Question 3
+// let a = parseInt(prompt("Enter a number: "))
+// if(a%2==0 && a%3==0){
+//     console.log("Divisible by 2 & 3")
+// }
+// else{
+//     console.log("Not divisible by 2 & 3")
+// }
+
+// //Question 4
+// let a = parseInt(prompt("Enter a number: "))
+// if(a%2==0 || a%3==0){
+//     console.log("Divisible by 2 or 3")
+// }
+// else{
+//     console.log("Not divisible by 2 or 3")
+// }
+
+// //Question 5
+// let age = parseInt(prompt("Your age is:"))
+// let x=(age>18)?"You can drive":"You cannot drive";
+// console.log(x);
